7d7367982e7bd41ffb2dedc7bb020f5b1f0a32ec
shinyfuwante <mr.philliptang@gmail.com>
2022-09-02 23:26:37 -0700

    Add linter, setup server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Dynamic UI",
-      template: "./src/template.html",
+      title: 'Dynamic UI',
+      template: './src/template.html',
     }),
   ],
 };
