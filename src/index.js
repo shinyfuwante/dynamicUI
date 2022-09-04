e6e97b691979287463fa07b6dbef24f5fbaf7840
shinyfuwante <mr.philliptang@gmail.com>
2022-09-03 20:13:45 -0700

    Create skeleton of dropdown menu

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@
 const mainElement = document.querySelector("div#content");
 
 /* eslint-disable no-console */
-// create the dropdown element
+// create the main dropdown element
 const dropdownMenu = document.createElement("div");
 dropdownMenu.classList.add("dropdown-menu");
 mainElement.appendChild(dropdownMenu);
-dropdownMenu.innerText("Hover me!");
-dropdownMenu.addEventListener("hover", console.log("dropdown element hovered"));
+dropdownMenu.innerText = "Hover me!";
+dropdownMenu.addEventListener("mouseover", (e) => {
+  if (!e.target.classList.contains("dropdown-menu")) return;
+  console.log("button hovered");
+  console.log(e.target);
+});
+
+// create the children of the dropdown element
+for (let i = 0; i < 3; i++) {
+  const element = document.createElement("div");
+  element.innerText = `List Item ${i + 1}`;
+  element.classList.add("dropdown-element");
+  dropdownMenu.appendChild(element);
+}
+// now to add css
 
 // 2) Mobile menu (like a hamburger menu)
 
