083996cd8027310af3032836fe924bdcc3e4def4
shinyfuwante <mr.philliptang@gmail.com>
2022-09-03 21:47:38 -0700

    Implement button tabs with stubbed code for hamburger and carousel

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
-import './styles/reset.css';
-import './styles/dropdown.css';
-// 1) Dropdown menu
-// element that on click, will display the list
-// can do on hover also.
+import "./styles/reset.css";
+import "./styles/dropdown.css";
+import initializeDropDown from "./dropdown";
+
 const mainElement = document.querySelector("div#content");
-
-/* eslint-disable no-console */
-// create the main dropdown element
-const dropdownMenu = document.createElement("div");
-dropdownMenu.classList.add("dropdown-menu");
-mainElement.appendChild(dropdownMenu);
-dropdownMenu.innerText = "Hover me!";
-dropdownMenu.addEventListener("mouseover", (e) => {
-  if (!e.target.classList.contains("dropdown-menu")) return;
-  console.log("button hovered");
-  console.log(e.target);
-});
-
-// create the children of the dropdown element
-for (let i = 0; i < 3; i++) {
-  const element = document.createElement("div");
-  element.innerText = `List Item ${i + 1}`;
-  element.classList.add("dropdown-element");
-  dropdownMenu.appendChild(element);
-}
-// now to add css
+// create tabs/buttons
+const buttons = (() => {
+  const dropdownButton = document.createElement("button");
+  const hamburgerButton = document.createElement("button");
+  const carouselButton = document.createElement("button");
+  const configButtons = () => {
+    dropdownButton.innerText = "Dropdown";
+    hamburgerButton.innerText = "Hamburger Menu";
+    carouselButton.innerText = "Carousel";
+  };
+  const renderButtons = () => {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+    buttonContainer.appendChild(dropdownButton);
+    buttonContainer.appendChild(hamburgerButton);
+    buttonContainer.appendChild(carouselButton);
+    configButtons();
+    mainElement.appendChild(buttonContainer);
+  };
+  const clearPage = () => {
+    while (mainElement.hasChildNodes()) {
+      mainElement.lastChild.remove();
+    }
+    renderButtons();
+  };
+  dropdownButton.addEventListener("click", () => {
+    clearPage();
+    initializeDropDown();
+    console.log('clicked dropdown');
+  });
+  hamburgerButton.addEventListener("click", () => {
+    clearPage();
+    // initializeDropDown();
+    console.log('clicked hamburger');
+  });
+  carouselButton.addEventListener("click", () => {
+    clearPage();
+    // initializeDropDown();
+    console.log('clicked carousel');
+  });
+  return {
+    renderButtons,
+  };
+})();
+buttons.renderButtons();
+initializeDropDown();
 
 // 2) Mobile menu (like a hamburger menu)
 
