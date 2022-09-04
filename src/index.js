8ae449837fb5615fc3df63f8e295e89e2d8e2278
shinyfuwante <mr.philliptang@gmail.com>
2022-09-04 13:16:42 -0700

    Remove unused hamburger menu button

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ const mainElement = document.querySelector("div#content");
 // create tabs/buttons
 const buttons = (() => {
   const dropdownButton = document.createElement("button");
-  const hamburgerButton = document.createElement("button");
   const carouselButton = document.createElement("button");
   const configButtons = () => {
     dropdownButton.innerText = "Dropdown";
-    hamburgerButton.innerText = "Hamburger Menu";
     carouselButton.innerText = "Carousel";
   };
   const renderButtons = () => {
     const buttonContainer = document.createElement("div");
     buttonContainer.classList.add("button-container");
     buttonContainer.appendChild(dropdownButton);
-    buttonContainer.appendChild(hamburgerButton);
     buttonContainer.appendChild(carouselButton);
     configButtons();
     mainElement.appendChild(buttonContainer);
@@ -33,11 +30,6 @@ const buttons = (() => {
     initializeDropDown();
     console.log('clicked dropdown');
   });
-  hamburgerButton.addEventListener("click", () => {
-    clearPage();
-    // initializeDropDown();
-    console.log('clicked hamburger');
-  });
   carouselButton.addEventListener("click", () => {
     clearPage();
     // initializeDropDown();
@@ -50,6 +42,4 @@ const buttons = (() => {
 buttons.renderButtons();
 initializeDropDown();
 
-// 2) Mobile menu (like a hamburger menu)
-
-// 3)Image slider/carousel
+// 2)Image slider/carousel
